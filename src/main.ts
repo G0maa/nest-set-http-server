73a8fe4588208dcdb688a7780898a6a94cb9fd71
Mohammad Gomaa <midomaxgomaa@gmail.com>
2025-06-30 10:25:37 +0300

    where is the bug?

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { INestApplicationContext } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { AppModule } from './app.module.js';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(
+  adapter: ExpressAdapter,
+): Promise<INestApplicationContext> {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(adapter));
+
+  const t = app.getHttpAdapter();
+  t.get('/test2', (req, res) => {
+    res.send('Hello Test2!');
+  });
+
+  return app;
 }
-bootstrap();
+
+async function init() {
+  const app = express();
+  const server = app.listen(3000, () => {
+    console.log('server started');
+  });
+
+  app.get('/test', (req, res) => {
+    // If adapter.setHttpServer(server) is NOT called
+    // This route should not work
+    // But it works either way.
+    res.send('Hello Test!');
+  });
+
+  const adapter = new ExpressAdapter(app);
+
+  // This line shouldn't be needed.
+  // adapter.setHttpServer(server);
+
+  const nest: INestApplicationContext = await bootstrap(adapter);
+
+  await nest.init();
+}
+init();
